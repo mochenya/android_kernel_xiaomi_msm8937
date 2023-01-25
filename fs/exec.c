016660f619ab59204bed282e37960aa09e0b689a
admin <paper@localhost>
2023-01-25 12:39:17 +0800

    drivers: kernelsu: Mandatory parameter passing in the kernel tree
    * Part 2 migration on this page: https://kernelsu.org/guide/how-to-integrate-for-non-gki.html
    * fs/exec.c Add back from this commit: https://github.com/aosp-mirror/kernel_common/commit/449325b5
    * fs/open.c Add back from this commit: https://github.com/aosp-mirror/kernel_common/commit/cbfe20f5
    * fs/read_write.c sync kernelsu changes
    * fs/stat.c Add back from this commit: https://github.com/aosp-mirror/kernel_common/commit/a528d35e
    
    备注: 理论来说兼容 4.4 4.9 3.18
    
    Co-authored-by: weishu <twsxtd@gmail.com>
    Change-Id: I170b3fbafc4b62bb719cc4f86c458f756c3b999e

--- fs/exec.c
+++ fs/exec.c
@@ -1673,6 +1673,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU_BACKPORT
+/* from kernel su */
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+#endif
 /*
  * sys_execve() executes a new program.
  */
@@ -1687,6 +1692,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 	bool is_su;
+
+#ifdef CONFIG_KSU_BACKPORT
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
