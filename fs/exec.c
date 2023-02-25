a5cb1056366d6fa7c833466758d7d8aea9039e5b
mochenya <mochen20070702@gmail.com>
2023-02-25 19:44:37 +0800

    Mi8937: Prepare for KernelSU To do that,you can see https://kernelsu.org/zh_CN/guide/how-to-integrate-for-non-gki.html to read more about it.

--- fs/exec.c
+++ fs/exec.c
@@ -1676,6 +1676,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1823,6 +1824,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (displaced)
 		put_files_struct(displaced);
 	return retval;
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 
 out:
 	if (bprm->mm) {
